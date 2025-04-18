d6fbd0a979e0004114d66ac545018d997a7c5192
SwordJack <49801226+SwordJack@users.noreply.github.com>
2025-04-18 16:43:38 -0500

    Start the locale module.

---
+++ ark/core/locale/__init__.py
@@ -0,0 +1,10 @@
+#! python3
+# -*- encoding: utf-8 -*-
+"""
+@File   :   __init__.py
+@Created:   2025/04/18 14:57
+@Author :   SwordJack
+@Contact:   https://github.com/SwordJack/
+"""
+
+# Here put the import lib.
