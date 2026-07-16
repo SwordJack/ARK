16c85ef721c8a59a5a65dc34abb7251186b9c1dd
SwordJack <49801226+SwordJack@users.noreply.github.com>
2026-07-17 02:13:41 +0800

    Create `database` module.

---
+++ isobase/database/__init__.py
@@ -0,0 +1,13 @@
+#! python3
+# -*- coding: utf-8 -*-
+"""This module supports database operations.
+
+@File   : __init__.py
+@Created: 2026/07/17 02:04 (UTC+08:00)
+@Author : SwordJack
+@Contact: https://github.com/SwordJack/
+"""
+
+# Here put the import lib.
+
+
